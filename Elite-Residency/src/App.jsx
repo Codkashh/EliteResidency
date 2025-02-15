2ce2506b6a0807f6c22a6c0cc8c44b46a86c78ab
Codkashh <as3733542@gmail.com>
2025-02-15 21:23:17 +0530

    footer component

--- Elite-Residency/src/App.jsx
+++ Elite-Residency/src/App.jsx
@@ -4,8 +4,10 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
@@ -15,6 +17,7 @@ const App = () => {
       <Projects/>
       <Testimonials/>
       <Contact />
+      <Footer/> 
     </div>
   )
 }
